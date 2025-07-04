77b9c862cea21db523a3cf587dc4b52d60c79f1c
Kranti Kumar Shah <Krantikumar803@gmail.com>
2025-07-04 15:09:57 +0545

    imporved error handlers

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,18 +1,22 @@
 import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import ErrorHandler from "../utils/errorHandler";
 
-const globalErrorhandler: ErrorRequestHandler = (
-  err: ErrorHandler, // using ErrorHandler class as a type here!
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  err.statusCode = err.statusCode || 500;
-  err.message = err.message || "Internal Server Error";
+const globalErrorhandler: ErrorRequestHandler = (err, req, res, next) => {
+  let message = "Internal Server Error";
+  let statusCode = 500;
 
-  res.status(err.statusCode).json({
+  if (err instanceof ErrorHandler) {
+    message = err.message || "Internal Server Error";
+    statusCode = err.statusCode || 500;
+  } else if (err instanceof Error) {
+    message = err.message;
+  } else if (typeof err === "string") { //this will handle something like this :=> throw "something went wrong";
+    message = err;
+  }
+
+  res.status(statusCode).json({
     success: false,
-    message: err.message,
+    message,
   });
 };
 
--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction, RequestHandler } from "express";
 
 const catchAsyncError =
   (passedFunc: RequestHandler): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) => {
+  (req, res, next) => {
     Promise.resolve(passedFunc(req, res, next)).catch((err) => next(err));
   };
 export default catchAsyncError;
